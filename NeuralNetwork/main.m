b5f9f21862ece41cb59c09f2f58963bf2282161c
XiaChao <small-wolf@live.cn>
2018-06-10 17:47:29 +0800

    删除了根目录下的main,将其移动到了神经网络文件夹，神经网络效果欠佳，只能达到52%的正确率

---
+++ NeuralNetwork/main.m
@@ -0,0 +1,94 @@
+%% 读取结果并存储
+grades=xlsread('grades.xlsx');
+grades=grades(:,2);%第一行为索引，第二行为结果
+%% 读取图片并存储
+TrainDatabasePath = uigetdir('.\datas\allpicture', 'Select training database path' );
+img=cell(1,500);
+% graImg=cell(1,500);
+row=500;
+col=300;
+
+graImg=zeros(row,col,500);
+for i=1:1:500
+    index=int2str(i);
+    str = strcat(TrainDatabasePath,'\SCUT-FBP-',index,'.jpg');
+    img{i}=imread(str);
+    img{i}=imresize(img{i},[row,col]);
+    %转化为双精度
+    graImg(:,:,i)=im2double(rgb2gray(img{i}));
+%     subplot(1,2,1),imshow(img{i});
+%     title(grades(i),'fontsize',10);
+%     subplot(1,2,2),imshow(graImg(:,:,i));
+%     title(grades(i),'fontsize',10);
+end
+
+
+m=400;
+
+%划分出训练集和测试集,并将评分规整为整数
+TrainingData=graImg(:,:,1:m);
+TrainingResult=round(grades(1:m));
+
+TestData=graImg(:,:,m+1:500);
+TestResult=round(grades(m+1:500));
+
+
+save 'datas.mat';
+% % 读取数据
+% clear all;clc;
+% load('datas.mat');
+
+%% 利用PCA把训练的数据处理完毕
+M=mean(TrainingData,3);
+
+
+%求图像散布矩阵Gt（总体散布矩阵）
+Gt=zeros(col,col);
+for i = 1 : m
+    temp = TrainingData(:,:,i)-M;
+    Gt =  Gt + temp'*temp;
+end
+Gt=Gt/m;
+d = 10;
+[V,~] = eigs(Gt,d);%求特征值和特征向量
+%ＰＣＡ空间投影图像
+V = orth(V); %求V的标准正交基
+ProjectedImages = zeros(row,d);
+%得到新的训练数据
+ConvertTrainingData=zeros(row*d,m);
+parfor i = 1 : m
+    ProjectedImages(:,:,i) = TrainingData(:,:,i)*V;
+    ConvertTrainingData(:,i)=reshape(ProjectedImages(:,:,i),[row*d,1])';
+end
+
+ConvertTrainingData=premnmx(ConvertTrainingData);
+
+%% 根据得到的数据，以及相应的结果，进行神经网络处理
+
+%构造输出矩阵
+class=5;%一共五种评分
+output=zeros(m,class);
+for i=1:1:m
+    output(i,TrainingResult(i))=1;
+end
+%创建神经网络
+net = newff( minmax(ConvertTrainingData) , [d 5] , { 'logsig' 'purelin' } , 'traingdx' ) ;
+
+%设置训练参数
+net.trainparam.show = 50 ;
+net.trainparam.epochs = 2000 ;
+net.trainparam.goal = 0.0001 ;
+net.trainParam.lr = 0.001 ;
+
+%开始训练
+%    The matrix format is as follows:
+%      X  - RxQ matrix
+%      Y  - UxQ matrix.
+%    Where:
+%      Q  = number of samples
+%      R  = number of elements in the network's input
+%      U  = number of elements in the network's output
+net = train( net, ConvertTrainingData  , output' ) ;
+
+% save 'training.mat';
+
---
+++ NeuralNetwork/mainTest.m
@@ -0,0 +1,47 @@
+% load('training.mat');
+row=500;
+col=300;
+%% 将测试的数据集转变为神经网络形式
+M=mean(TestData,3);
+
+m=size(TestData,3);
+
+%求图像散布矩阵Gt（总体散布矩阵）
+Gt=zeros(col,col);
+for i = 1 : m
+    temp = TestData(:,:,i)-M;
+    Gt =  Gt + temp'*temp;
+end
+Gt=Gt/m;
+d = 10;
+[V,~] = eigs(Gt,d);%求特征值和特征向量
+%ＰＣＡ空间投影图像
+V = orth(V); %求V的标准正交基
+ProjectedImages = zeros(row,d);
+%得到新的训练数据
+ConvertTestData=zeros(row*d,m);
+parfor i = 1 : m
+    ProjectedImages(:,:,i) = TestData(:,:,i)*V;
+    ConvertTestData(:,i)=reshape(ProjectedImages(:,:,i),[row*d,1])';
+end
+ConvertTestData=premnmx(ConvertTestData);
+%构造输出矩阵
+class=5;%一共五种评分
+output=zeros(m,class);
+for i=1:1:m
+    output(i,TestResult(i))=1;
+end
+%% 仿真运算
+Y = sim( net , ConvertTestData );
+%统计识别正确率
+
+%output为相应的m*5，因此y也要为m*5
+Y=Y';%得到m*5个结果
+hitNum = 0 ;
+for i=1:1:m
+    [~ , Index] = max( Y( i ,  : ) ) ;
+    if output(i,Index)==1
+        hitNum = hitNum + 1 ;
+    end
+end
+sprintf('识别率是 %3.3f%%',100 * hitNum / m )
